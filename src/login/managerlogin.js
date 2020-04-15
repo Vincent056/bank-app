f5e46912852ba5efa86375de32859a5e3069fec4
Trevor Glassey <tkglassey@gmail.com>
2020-04-15 16:17:24 -0700

    Updated Managerlogin
    update to managerlogin to test certification.

--- src/login/managerlogin.js
+++ src/login/managerlogin.js
@@ -1,16 +1,57 @@
 import React from 'react';
+import axios from 'axios';
 import { Redirect} from 'react-router-dom';
 
 class ManagerLogin extends React.Component {
-    state ={
-        loggedIn: false,
-		customerLogin: false,
+    constructor(){
+        super()
+        this.state ={
+            username: "",
+            password: "",
+            temp: "ManagerLogin",
+            loggedIn: false,
+            customerLogin: false,
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-    handleSubmit= () => {
-        //***********code to check databased data here
+    
+    handleChange(event){
+        const {name, value} = event.target
         this.setState({
-            loggedIn: true
+            [name]: value
         })
+    }
+	
+	handleSubmit(event){
+        event.preventDefault();
+		
+		let loginInfo = new FormData();
+		
+		loginInfo.append('username', this.state.username);
+        loginInfo.append('password', this.state.password);
+		
+		axios({
+            method: 'post',
+            url: 'http://bank.cvs3.com/bank-app/api/managerlogin.php',
+            data: loginInfo,
+            config: {headers: {'Content-Type': 'x-www-form-urlencoded'}}
+        }).then((response) => {
+            console.log(response);
+			
+			//Successful Log in
+            if(response.data.id){
+                this.setState({
+                    loggedIn: true
+                })
+            }
+			
+			}).catch(function(error) {
+            // handle error
+            console.log(error)
+        });
+        
+        
     }
 	customerLog = () => {
 		this.setState({
@@ -20,20 +61,30 @@ class ManagerLogin extends React.Component {
     render(){
         if (this.state.loggedIn ){
 			//***********code to check databased data here
-            //return <Redirect to="/managerMain/"/>
+            return <Redirect to="/signUp/"/>
         } 
 		if (this.state.customerLogin ){
             return <Redirect to="/"/>
-        } 
-        
-        return(
+        }
+		
+		return(
 			<div>
 				<form >
 					<h1>Manager Login</h1>
 					<label>Username</label><br></br>
-					<input type ='text' name='username'></input><br></br>
+                    <input 
+                            type ='text' 
+                            name='username' 
+                            value={this.state.username} 
+                            onChange={this.handleChange} 
+                        ></input><br></br>
 					<label>Password</label><br></br>
-					<input type ='password' name = 'password'></input><br></br>
+                    <input 
+                            type ='password' 
+                            name = 'password' 
+                            value={this.state.password}
+                            onChange={this.handleChange} 
+                        ></input><br></br>
 					<button onClick={this.handleSubmit}>Submit</button>
 				</form>
 				<div>
@@ -43,5 +94,4 @@ class ManagerLogin extends React.Component {
         )
     }
 }
-
-export default ManagerLogin
+export default ManagerLogin        
