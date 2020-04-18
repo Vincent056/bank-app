93aff6b193eec414b87b66df19aa1a9aa5df0108
Vincent Shen <47534281+Vincent056@users.noreply.github.com>
2020-04-18 02:47:01 -0700

    Update billingscript.php

--- api/billingscript.php
+++ api/billingscript.php
@@ -13,7 +13,8 @@
 
     
     $today = date('Y-m-d');
-    echo "$today";
+    echo date("Y-m-d h:i:sa") . ": script started";
+    echo "<br>";
     $query = "SELECT * FROM automated_billing WHERE start_date <= '$today' AND end_date >= '$today' AND last_date < '$today' or last_date is NULL;";
     $result = $conn -> query($query);
     if (!$result) die (mysql_fatal_error());
@@ -21,10 +22,10 @@
     $query2 = "";
     for ($j = 0 ; $j < $rows ; ++$j)
     {
-        echo "<br>";
+        
         $result->data_seek($j);
         $row = $result->fetch_array(MYSQLI_NUM);
-        echo "from $row[6] to $row[1] at $row[2]";
+        echo  date("Y-m-d h:i:sa") . " from $row[6] to $row[1] at $row[2]";
         $query2 .= "INSERT INTO transaction (transaction_id, routing_number, recipient, recipient_account_num, transaction_type, amount, date, bank_account_account_id) VALUES (NULL, NULL, NULL, '$row[1]', 'automated_billing', '$row[2]', current_timestamp(), '$row[6]');";
         $query2 .= "UPDATE bank_account SET balance = balance - $row[2] WHERE bank_account.account_id = '$row[6]';";
         $query2 .= "UPDATE bank_account SET balance = balance + $row[2] WHERE bank_account.account_id = '$row[1]';";
