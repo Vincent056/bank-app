efd5b8a195981bb2e739e37bff5561515026eb8c
Vincent Shen <47534281+Vincent056@users.noreply.github.com>
2020-04-18 03:35:46 -0700

    Update openbank.php

--- api/openbank.php
+++ api/openbank.php
@@ -1,12 +1,12 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-$servername = "localhost";
-$user = "root";
-$password = "cs160team2%";
-$db = "BankingDB";
+    require_once 'login.php';
+    header('Access-Control-Allow-Origin: *');
 
-$conn = new mysqli($servername, $user, $password, $db) or die("Connect failed: %s\n".$conn->error);
+    
+    $conn = new mysqli($servername, $user, $password, $db);
+    if($conn->connect_error) die($conn->connect_error);
+
 
 if (isset($_POST['id']) && isset($_POST['acctype'])){
     $id = $_POST['id'];
