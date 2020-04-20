d990459ed08d6bcf7dc7d8b2c8d22fc4f53bb0d4
sieun <sieunhim210@yahoo.com>
2020-04-19 17:46:27 -0700

    update files

--- api/trasnferfund.php
+++ api/trasnferfund.php
@@ -7,12 +7,12 @@
     $conn = new mysqli($servername, $user, $password, $db);
     if($conn->connect_error) die($conn->connect_error);
 
-if (isset($_POST['id']) && isset($_POST['acct_num']) && isset($_POST['amount'])){
+if (isset($_POST['id']) && isset($_POST['acct_num']) && isset($_POST['amount'])&& isset($_POST['receiver'])){
 
      $id = mysql_entities_fix_string($conn, $_POST['id']);
      $acct_num = mysql_entities_fix_string($conn, $_POST['acct_num']);
      $amount = mysql_entities_fix_string($conn, $_POST['amount']);
-
+     $receiver = mysql_entities_fix_string($conn, $_POST['receiver']);
      $query = "SELECT balance FROM bank_account WHERE account_id = '$id'";
      $result = $conn->query($query);
      $result->data_seek(0);
@@ -23,9 +23,9 @@ if (isset($_POST['id']) && isset($_POST['acct_num']) && isset($_POST['amount']))
                 echo "The transfer amount excceds available balance, Please transfer a smaller amount!";
             }else
             {
-                if (isset($_POST['routing']) && isset($_POST['receiver'])){ //external
+                if (isset($_POST['routing']) ){ //external
                     $routing = mysql_entities_fix_string($conn, $_POST['routing']);
-                    $receiver = mysql_entities_fix_string($conn, $_POST['receiver']);
+                   
                     $trans_type = "External";
                     $query2 = "INSERT INTO transaction (routing_number, recipient, recipient_account_num, transaction_type, amount, date, bank_account_account_id) 
                                 VALUES ('$routing','$receiver','$acct_num',$trans_type','$amount',current_timestamp(),'$id')";
@@ -34,7 +34,7 @@ if (isset($_POST['id']) && isset($_POST['acct_num']) && isset($_POST['amount']))
                 else{ //internal
                     $trans_type = "Internal";
                     $query2 = "INSERT INTO transaction (routing_number, recipient, recipient_account_num, transaction_type, amount, date, bank_account_account_id) 
-                                VALUES (NULL,NULL,'$acct_num','$trans_type','$amount',current_timestamp(),'$id');";
+                                VALUES (NULL,'$receiver','$acct_num','$trans_type','$amount',current_timestamp(),'$id');";
                     $query2 .= "UPDATE bank_account SET balance = balance - $amount WHERE bank_account.account_id = '$id';";
                     $query2 .= "UPDATE bank_account SET balance = balance + $amount WHERE bank_account.account_id = '$acct_num';";
                     echo "internal transfered";
