16e88eded614c4cffbc0e56258bb2813c5d36b50
sieun <sieunhim210@yahoo.com>
2020-04-21 22:19:07 -0700

    update

--- src/user_comp/atm.js
+++ src/user_comp/atm.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Redirect} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 
-class ATM extends React.Component{
-    constructor(props){
+class ATM extends React.Component {
+    constructor(props) {
         super(props)
-        this.state ={
+        this.state = {
             accounts: this.props.location.state.customer_accounts,
             accid: '',
             amount: 0,
@@ -15,77 +15,83 @@ class ATM extends React.Component{
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+       
+
     }
 
-    handleCancel =() =>{
+    checkInput() {
+        if (this.state.file.size < 0 ||
+            this.state.amount) return "E"
+    }
+    handleCancel = () => {
         this.setState({
             goback: true
         })
     }
-    
-    handleSubmit= (event) => {
+
+    handleSubmit = (event) => {
         event.preventDefault();
-       
+
         let userInfo = new FormData();
-        userInfo.append('acc_id',this.state.accid)
-        userInfo.append('amount',this.state.amount)
-        userInfo.append('type',this.state.type)
+        userInfo.append('acc_id', this.state.accid)
+        userInfo.append('amount', this.state.amount)
+        userInfo.append('type', this.state.type)
         axios({
             method: 'post',
             url: 'https://bank.cvs3.com/bank-app/api/atmtransaction.php',
             data: userInfo,
-            config: {headers: {'Content-Type': 'x-www-form-urlencoded'}}
-        }).then( (response) => {  
+            config: { headers: { 'Content-Type': 'x-www-form-urlencoded' } }
+        }).then((response) => {
             // handle success
             console.log(response.data)
             this.setState({
                 message: "Transaction Complete!",
             })
-        }).catch(function(error) {
+        }).catch(function (error) {
             // handle error
             console.log(error)
         })
-        
+
     }
     handleChange = (event) => {
-        const {name, value} = event.target
+        const { name, value } = event.target
         this.setState({
             [name]: value
         })
     }
 
-    render(){
-        if(this.state.goback){
-            return <Redirect to='/usermain/'/>
+    render() {
+        if (this.state.goback) {
+            return <Redirect to='/usermain/' />
         }
-        return(
+        return (
             <div>
                 <form>
                     <h1>ATM</h1>
                     <label>Choose An Account</label><br></br>
-                    <select name = 'accid'
-                            value = {this.state.accfrom}
-                            onChange={this.handleChange} >
-                                {this.state.accounts.map(account => (
-                            <option key = {account.account_id}
-                                    value= {account.account_id}>{account.account_id} 
+                    <select name='accid'
+                        value={this.state.accfrom}
+                        onChange={this.handleChange} >
+                        {this.state.accounts.map(account => (
+                            <option key={account.account_id}
+                                value={account.account_id}>{account.account_id}
                                             - {account.account_type}: {account.balance} </option>
-                                ))}    
-                        </select><br></br><br></br>
-                        <select  name = 'type'
-                            value = {this.state.type}
-                            onChange={this.handleChange} >
-                                <option value = ''>Deposit/Withdraw</option>
-                                <option value = 'deposit'>Deposit</option>
-                                <option value = 'withdraw'>Withdraw</option> 
-                        </select><br></br>
+                        ))}
+                    </select><br></br><br></br>
+                    <select name='type'
+                        value={this.state.type}
+                        onChange={this.handleChange} >
+                        <option value=''>Deposit/Withdraw</option>
+                        <option value='deposit'>Deposit</option>
+                        <option value='withdraw'>Withdraw</option>
+                    </select><br></br>
                     <label>Amount</label><br></br>
-                    <input type ='text' 
-                            onChange ={this.handleChange}
-                            name = 'amount'
-                            value = {this.state.amount}></input>
-                    <button onClick ={this.handleSubmit}>Go</button>
-                    <button onClick ={this.handleCancel}>Back To Bank</button>
+                    <input type='text'
+                        onChange={this.handleChange}
+                        name='amount'
+                        value={this.state.amount}></input>
+                    <button onClick={this.handleSubmit}>Go</button>
+                    <button onClick={this.handleCancel}>Back To Bank</button>
                 </form>
                 {this.state.message}
             </div>
--- src/user_comp/depositcheck.js
+++ src/user_comp/depositcheck.js
@@ -1,81 +1,118 @@
 import React from 'react';
-import { Redirect} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 import styles from './../mystyle.module.css';
 
-class Check extends React.Component{
-    constructor(props){
+class Check extends React.Component {
+    constructor(props) {
         super(props)
-        this.state ={
+        this.state = {
             accid: this.props.accounts[0].account_id,
-            amount: 0,
+            amount: 0.00,
+            preview_file: null,
+            file: null,
             message: ''
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.previewImage = this.previewImage.bind(this);
     }
-    
-    handleSubmit= (event) => {
-        event.preventDefault();
-       
-        let userInfo = new FormData();
-        userInfo.append('acc_id',this.state.accid)
-        userInfo.append('amount',this.state.amount)
-        userInfo.append('type','deposit')
-        axios({
-            method: 'post',
-            url: 'https://bank.cvs3.com/bank-app/api/atmtransaction.php',
-            data: userInfo,
-            config: {headers: {'Content-Type': 'x-www-form-urlencoded'}}
-        }).then( (response) => {  
-            // handle success
-            console.log(response.data)
-            this.setState({
-                message: "Deposit Complete!"
-            })
-        }).catch(function(error) {
-            // handle error
-            console.log(error)
+    checkInput() {
+        if (this.state.file === null
+            || this.state.amount === 0.00) return "E"
+        else if (this.state.file.type !== "image/jpeg"
+            && this.state.file.type !== "image/png"
+            && this.state.file.type !== "image/jpg") return "I"
+        else if (this.state.amount < 0) return "A"
+        else return "OK"
+    }
+    previewImage = (event) => {
+        this.setState({
+            preview_file: URL.createObjectURL(event.target.files[0]),
+            file: event.target.files[0]
         })
-        
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        let check = this.checkInput();
+        if (check === "OK") {
+            let userInfo = new FormData();
+            userInfo.append('acc_id', this.state.accid)
+            userInfo.append('amount', this.state.amount)
+            userInfo.append('type', 'deposit')
+            axios({
+                method: 'post',
+                url: 'https://bank.cvs3.com/bank-app/api/atmtransaction.php',
+                data: userInfo,
+                config: { headers: { 'Content-Type': 'x-www-form-urlencoded' } }
+            }).then((response) => {
+                // handle success
+                console.log(response.data)
+                this.setState({
+                    message: "Deposit Complete!"
+                })
+            }).catch(function (error) {
+                // handle error
+                console.log(error)
+            })
+        }
+        else if (check === "E") {
+            this.setState({
+                message: "One of the fields is empty!"
+            })
+        }
+        else if (check === "I") {
+            this.setState({
+                message: "Invalid file type!"
+            })
+        }
+        else if (check === "A") {
+            this.setState({
+                message: "Invalid amount!"
+            })
+        }
     }
     handleChange = (event) => {
-        const {name, value} = event.target
+        const { name, value } = event.target
         this.setState({
             [name]: value
         })
     }
-    handleCancel =() =>{
+    handleCancel = () => {
         this.props.goback()
     }
-    render(){
-        
-        return(
+    render() {
+
+        return (
             <div className={styles.center}>
-			<div className={styles.topnav}>
-				<a><button className={styles.buttontopnav} onClick={this.handleCancel}>Back</button></a>
-			</div>
-            <form className={styles.billing}>
-                <h1>Deposit Check</h1>
-                <label>Choose An Account</label><br></br>
-                <select  name = 'accid'
-                        value = {this.state.accfrom}
+                <div className={styles.topnav}>
+                    <a><button className={styles.buttontopnav} onClick={this.handleCancel}>Back</button></a>
+                </div>
+                <form className={styles.billing}>
+                    <h1>Deposit Check</h1>
+                    <input type='file'
+                        onChange={this.previewImage}
+                        name='file'></input><br></br>
+                    <img src={this.state.preview_file} width='400' height='200' /><br></br>
+                    <label>Choose An Account</label><br></br>
+                    <select name='accid'
+                        value={this.state.accfrom}
                         onChange={this.handleChange} >
-                             {this.props.accounts.map(account => (
-                           <option key = {account.account_id}
-                                value= {account.account_id}>{account.account_id} 
+                        {this.props.accounts.map(account => (
+                            <option key={account.account_id}
+                                value={account.account_id}>{account.account_id}
                                         - {account.account_type}: {account.balance} </option>
-                            ))}    
+                        ))}
                     </select><br></br><br></br>
-                    
-                <label>Amount</label><br></br>
-                <input type ='text' 
-                        onChange ={this.handleChange}
-                        name = 'amount'
-                        value = {this.state.amount}></input> <br/>
-                <button onClick ={this.handleSubmit}>Deposit</button>
-            </form>
-            {this.state.message}
+                    <label>Amount</label><br></br>
+                    <input type='number' step='0.01'
+                        onChange={this.handleChange}
+                        name='amount'
+                        value={this.state.amount}></input>
+                    <button onClick={this.handleSubmit}>Deposit</button>
+                    <p className = 'error'>{this.state.message}</p>
+                </form>
+                
             </div>
         )
     }
