29735f8d8dc121f4227a271f85feaf1d75e11e04
Vincent Shen <47534281+Vincent056@users.noreply.github.com>
2020-04-27 04:45:24 -0700

    Update managerquery.php

--- api/managerquery.php
+++ api/managerquery.php
@@ -11,9 +11,9 @@
         $input = mysql_entities_fix_string($conn, $_POST['input']);
         $attribute = mysql_entities_fix_string($conn, $_POST['attribute']);
         $man_id = mysql_entities_fix_string($conn, $_POST['man_id']);
-        if($attribute == "1")
+        if(strcmp($attribute,"1"))
         {
-            $input == "1";
+            $input == 1;
         }
         $query = "SELECT customer.customer_id as customer_id, address_id, username, first_name, last_name, street, apartment_number, city, state, zip_code, email, phone, balance
         FROM customer LEFT OUTER JOIN address ON customer.address_address_id = address.address_id LEFT OUTER JOIN balance_total ON customer.customer_id = balance_total.customer_id
